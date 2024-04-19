9e05495c45d29264c3c0d9f3267724bc41af302e
Guilherme Swerts de Oliveira <p.guilherme.swerts@viveo.com.br>
2024-04-19 11:21:11 -0300

    .

--- carvao-app/ClientApp/src/util/GetDataUser.jsx
+++ carvao-app/ClientApp/src/util/GetDataUser.jsx
@@ -1,7 +1,17 @@
 import { eTipoUsuario } from '../enum/eTipoUsuario';
 export const GetDataUser = () => {
     let access_token = window.localStorage.getItem("access_token");
-    if (!access_token) window.location.href = '/login';
+    if (!access_token) {
+        if (window.location.pathname.toUpperCase() != "/LOGIN")
+            window.location.href = '/login';
+        else
+            return {
+                TipoUsuario: 0,
+                UsuarioId: 0,
+                IsMaster: false
+            }
+    }
+
     const data = JSON.parse(atob(access_token.split('.')[1]));
     const split = JSON.stringify(data).split(',');
     let id = 0;
